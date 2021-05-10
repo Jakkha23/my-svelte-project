5b61b980b2ecc01d2c0ffaf368e6fd35de0c91b3
Jakkha23 <jakob.khaytan@skola.botkyrka.se>
2021-05-10 16:46:48 +0200

    testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,30 @@
 <script>
-	export let name;
+	let name = "jakob";
+	let CircleX = 1
+	let CircleY = 1
+	let count = 0
+
+	function increase() {
+		count = count +1
+	}
+	function reset() {
+		count = 0
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button class="btn1" on:click={increase}>Click me</button>
+	<button class="btn2" on:click={increase}>click me too</button>
+	<button class="btn3" on:click={reset}>'SUBTLE OFF-WHITE COLORING'</button>
+	<p>{count}</p>
 </main>
 
 <style>
+	main {
+		background-color: #f8f8ff;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,10 +33,21 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #fc7b03;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	.btn1 {
+		background-color: #fc7b03;
+	}
+	.btn2 {
+		background-color: aquamarine;
+	}
+	.btn3 {
+		background-color: 	#f8f8ff;
+		border-radius: 12px;
 	}
 
 	@media (min-width: 640px) {
